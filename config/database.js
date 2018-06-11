5f6759114cb364bd26f10036d0d3f5067329dd54
shanu302 <30571196+shanu302@users.noreply.github.com>
2018-06-11 14:24:09 +0530

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,13 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': '127.0.0.1', 
+        'user': 'root', 
+        'password': 'password', 
+        'database': 'dbname'
+    },
+	'database': 'dbname',
+    
+};
+
+	
